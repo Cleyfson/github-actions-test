7bb1480644b97ab434dbd7531f368cd9a08cbe58
Cleifson <cl.raujo1@gmail.com>
2024-06-07 22:16:50 -0300

    feat: add jwt register

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+    Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::group(['middleware' => 'auth.jwt'], function () {
+        Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    });
 });
